03f10e69a8088afce442f1c471c6057b9d236bf2
Richard Rodger <richard@ricebridge.com>
2013-05-09 22:52:12 +0100

    bump 0.2.2

--- test/travis.js
+++ test/travis.js
@@ -4,23 +4,6 @@
 var assert = require('assert')
 var _      = require('underscore')
 
-var mock = {
-  cmd: {},
-  add: function(spec,func){ this.cmd[spec.cmd]=func },
-  make: function() { return {} },
-  util: {deepextend: function(first){return first}},
-  pin: function(){return {entity:function(){}}},
-  err: function(){}
-}
 
-var user_plugin = require('..')
+var auth_plugin = require('..')
 
-user_plugin(mock,{},function(){
-  var rand = Math.random()
-  var start = new Date().getTime()
-  mock.cmd.ping({rand:rand},function(err,res){
-    assert.ok(null==err)
-    assert.equal(res.rand,rand)
-    assert.ok(start<=res.when)
-  })
-})
