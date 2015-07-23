6e20d4141e738110691e83d9e2b21a50cd9ddec5
mirceaalexandru <mircea.alexandru@gmail.com>
2015-07-23 19:56:38 +0300

    added unit tests that uses also the example plugin for restrict-login

--- test/restrict-login.test.js
+++ test/restrict-login.test.js
@@ -14,9 +14,9 @@ var util = require('./util.js')
 
 var cookie
 var user = {nick:'u1',name:'nu1',email:'u1@example.com',password:'u1',active:true}
-var seneca
 
 suite('restrict suite tests ', function() {
+  var seneca
   before({}, function(done){
     util.init(function(err, agentData, si){
       agent = agentData
@@ -86,7 +86,78 @@ suite('restrict suite tests ', function() {
         done(err)
       })
   })
+
+
+  test('api/login test with external restrict plugin', function(done) {
+    seneca.add({role: 'auth', restrict: 'login'}, function(args, done){
+      done(null, {ok: false, why: 'not-allowed'})
+    })
+
+    agent
+      .post('/auth/login')
+      .send({nick: user.nick, password: user.password})
+      .expect(401)
+      .end(function (err, res){
+        util.log(res)
+        assert(!res.body.ok, 'OK response')
+        assert.equal('not-allowed', res.body.why)
+        done(err)
+      })
+  })
 })
 
+suite('restrict suite tests ', function() {
+  var seneca
+  before({}, function(done){
+    util.init(function(err, agentData, si){
+      agent = agentData
+      seneca = si
+      seneca.use('auth-restrict-login')
+      done()
+    })
+  })
+
+  test('auth/register user', function(done) {
+    agent
+      .post('/auth/register')
+      .send(user)
+      .expect(200)
+      .end(function (err, res){
+        util.log(res)
+        assert(res.body.ok, 'Not OK')
+        assert(res.body.user, 'No user in response')
+        assert(res.body.login, 'No login in response')
+        cookie = util.checkCookie(res)
+        done(err)
+      })
+  })
+
+  test('api/login test with external restrict plugin - login not allowed', function(done) {
+    agent
+      .post('/auth/login')
+      .send({nick: user.nick, password: user.password})
+      .expect(401)
+      .end(function (err, res){
+        util.log(res)
+        assert(!res.body.ok, 'OK response')
+        assert.equal('not-allowed', res.body.why)
+        done(err)
+      })
+  })
+
+  test('api/login test with external restrict plugin - login allowed', function(done) {
+    agent
+      .post('/auth/login')
+      .send({nick: user.nick, password: user.password})
+      .set('Cookie', ['allow_login=' + 1])
+      .expect(200)
+      .end(function (err, res){
+        util.log(res)
+        assert(res.body.ok, 'OK response')
+        assert(res.body.user)
+        done(err)
+      })
+  })
+})
 
 
