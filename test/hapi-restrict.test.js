a4f28869cd4796ce915f9ea0e3ca7fb81a2dcacb
malex <mircea.alexandru@gmail.com>
2016-01-13 12:32:10 +0200

    small fix and added restrict test

---
+++ test/hapi-restrict.test.js
@@ -0,0 +1,82 @@
+'use strict'
+
+var Assert = require('assert')
+var agent
+
+var Lab = require('lab')
+var lab = exports.lab = Lab.script()
+var suite = lab.suite
+var test = lab.test
+var before = lab.before
+var after = lab.after
+
+var Util = require('./hapi-util.js')
+
+
+suite('Hapi restrict suite tests ', function () {
+  var cookie
+  var server
+  var user = {nick: 'u1', name: 'nu1', email: 'u1@example.com', password: 'u1', active: true}
+  var changed_user = {nick: 'u1', name: 'nu2', email: 'u1@example.com'}
+  var cookie
+
+  before({}, function (done) {
+    Util.init({}, function (err, srv) {
+      Assert.ok(!err)
+
+      server = srv
+      done()
+    })
+  })
+
+  after({}, function (done) {
+    server.seneca.close()
+    done()
+  })
+
+  test('failed api/service restrict test', function (done) {
+    var url = '/api/service'
+
+    server.inject({
+      url: url,
+      method: 'GET'
+    }, function (res) {
+      Assert.equal(302, res.statusCode)
+
+      done()
+    })
+  })
+
+  test('auth/register test', function (done) {
+    var url = '/auth/register'
+
+    server.inject({
+      url: url,
+      method: 'POST',
+      payload: user
+    }, function (res) {
+      Assert.equal(200, res.statusCode)
+      Assert(JSON.parse(res.payload).ok)
+      Assert(JSON.parse(res.payload).user)
+      Assert(JSON.parse(res.payload).login)
+
+      cookie = Util.checkCookie(res)
+
+      done()
+    })
+  })
+
+  test('failed api/service restrict test', function (done) {
+    var url = '/api/service'
+
+    server.inject({
+      url: url,
+      method: 'GET',
+      headers: { cookie: 'seneca-login=' + cookie }
+    }, function (res) {
+      Assert.equal(200, res.statusCode)
+
+      done()
+    })
+  })
+})
